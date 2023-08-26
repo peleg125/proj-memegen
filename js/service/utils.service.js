d95bc589d83f53e2d35c95443456fa4469cac66c
peleg125 <peleg@mischel.biz>
2023-08-26 20:04:17 +0300

    commit utils

---
+++ js/service/utils.service.js
@@ -0,0 +1,13 @@
+"use strict"
+
+function makeId(length = 6) {
+	var id = ""
+	var possible =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+	for (var i = 0; i < length; i++) {
+		id += possible.charAt(getRandomInt(0, possible.length))
+	}
+
+	return id
+}
